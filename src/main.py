2bfca180af81c41480ff88923f5b852a51237371
ottettus <etrangere767@gmail.com>
2025-02-08 23:12:43 +0100

    Addded feature than can draw rectangle around faces

--- src/main.py
+++ src/main.py
@@ -15,23 +15,28 @@ class FaceRecognizer:
             ret, frame = self.video_capture.read()
             if not ret:
                 raise("Can't receive frame")
-            cv2.imshow('Podglad', frame)
+            face = self.detect_faces(frame)
+            face_with_rectangle = self.draw_faces(face)
+
+            
 
 
 
-    def detect_faces(self):
-        pass
+    def detect_faces(self, frame):
+
+        rgb_frame = frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_frame)
+        return face_locations 
 
 
-    def draw_faces(self):
-        pass
+
+    def draw_faces(self, faces_location):
+        for x, y, w, h in faces_location:
+            return cv2.rectangle(faces_location,(x, y),(x + w, y + h), (0, 255, 0), 2)
 
     def cleanup(self):
-        self.video_capture.release()
-        cv2.destroyAllWindows()
+        pass
 
 if __name__ == "__main__":
-    face_recognizer = FaceRecognizer()
-    face_recognizer.start_capture()
-    face_recognizer.cleanup()
+    pass
     
